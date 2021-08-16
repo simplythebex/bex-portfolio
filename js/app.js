e3d69c116dfaa246c730f87d2503117a1cb2851e
simplythebex <becca.jones@live.co.uk>
2021-08-16 20:26:07 +0100

    styling and started responsiveness

--- js/app.js
+++ js/app.js
@@ -1,1 +1,18 @@
-console.log('hello')
+const aboutButton = document.querySelector('.about-button')
+const projectsButton = document.querySelector('.projects-button')
+const backgroundButton = document.querySelector('.background-button')
+const contactButton = document.querySelector('.contact-button')
+
+const scrollTo = (event) => {
+  console.log('scrolling')
+  const target = event.target.id
+  console.log(target)
+  document.querySelector(`.${target}`).scrollIntoView({
+    behavior: 'smooth'
+  })
+}
+
+aboutButton.addEventListener('click', scrollTo)
+projectsButton.addEventListener('click', scrollTo)
+backgroundButton.addEventListener('click', scrollTo)
+contactButton.addEventListener('click', scrollTo)
